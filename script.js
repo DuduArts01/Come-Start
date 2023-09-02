934a3297bbf6badd61514c7ca72d3fface743f74
Eduardo <108435809+DuduArts01@users.noreply.github.com>
2023-09-01 23:10:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ function count(){
     var end = document.getElementById("endtxt")
     var step = document.getElementById("steptxt")
     var res = document.querySelector("div#res")
-
-    res.innerHTML = "Counting: "
-   
+  
 //Variables loop
     var s = Number(start.value)
     //start
@@ -19,15 +17,18 @@ function count(){
     } else if(st <= 0){
         window.alert("[ERROR] Step cannot be less than or equal to 0")
     } else if(s < e){
+        res.innerHTML = "Counting: "        
        for(var c = s; c <= e; c += st){
             res.innerHTML += `${c} \u{1F449} `
         }
         //count up
+        res.innerHTML += "\u{1F3C1}"
     } else{
+        res.innerHTML = "Counting: "
         for(var c = s; c >= e; c -= st){
             res.innerHTML += `${c} \u{1F449} `
         }
         //count down
+        res.innerHTML += "\u{1F3C1}"
     }
-    res.innerHTML += "\u{1F3C1}"
 }
